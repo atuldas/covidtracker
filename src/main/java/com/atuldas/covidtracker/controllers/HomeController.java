49bea3bfe092ad0bdeb3d465b5219744124b7364
ADMIN <ADMIN@ATUL-DAS>
2021-05-12 00:45:29 +0530

    Tracker

---
+++ src/main/java/com/atuldas/covidtracker/controllers/HomeController.java
@@ -0,0 +1,32 @@
+package com.atuldas.covidtracker.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.atuldas.covidtracker.models.LocationStats;
+import com.atuldas.covidtracker.services.CovidTrackerDataService;
+
+@Controller
+public class HomeController {
+	
+	@Autowired
+	CovidTrackerDataService covidTrackerDataService;
+	
+	@GetMapping("/")
+	public String home(Model model) {
+        List<LocationStats> allStats = covidTrackerDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+   
+
+        return "home";
+    }
+
+}
